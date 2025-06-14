c680f97645f25ae5eb39b3a5f914ed550e24c37c
Kallebe Dantas <kallmonte@gmail.com>
2025-06-14 20:28:31 -0300

    Fix vercel vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
     // Load env file based on `mode` in the current working directory.
     // Set the third parameter to '' to load all env regardless of the
     // `VITE_` prefix.
-    const env = loadEnv(mode, process.cwd(), '');
     return {
         // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
         assetsInclude: ['**/*.jpg', '**/*.svg'],
         define: {
-            __APP_ENV__: JSON.stringify(env.APP_ENV),
+            __APP_ENV__: process.env.VITE_VERCEL_ENV,
         },
         build: {
             outDir: 'build',
