e080fb428df6b1f31cd16e9adc43080c68736442
Kallebe Dantas <kallmonte@gmail.com>
2025-06-14 20:15:04 -0300

    Fixing vite env loading

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
-    assetsInclude: ['**/*.jpg', '**/*.svg'],
-    define: {
-      'process.env': {},
-    },
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-  };
+export default defineConfig(({ mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the
+    // `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
+        assetsInclude: ['**/*.jpg', '**/*.svg'],
+        define: {
+            __APP_ENV__: JSON.stringify(env.APP_ENV),
+        },
+        build: {
+            outDir: 'build',
+        },
+        plugins: [react()],
+    };
 });
